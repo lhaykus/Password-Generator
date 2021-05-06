7ae13cd478789773728f0b03acdd312bb2b9098b
Loryn <lorynhaykus@yahoo.com>
2021-05-05 23:40:49 -0600

    creating if statements

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const specialCharacters= ["!", "@", "#", "$", "%", "^","&", "*", "(", ")", "-", "_", "=", "+","?",  ]
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "?",]
 //"',-./:;<>?@[\]_`{|}~
 
 //state variables
@@ -15,37 +15,37 @@ const specialCharacters= ["!", "@", "#", "$", "%", "^","&", "*", "(", ")", "-", 
 //check to make sure numbers are between 8-128, create if statements for if its not met
 //check to make sure at least one of the characters is chosen, if not create alert to make them choose something
 //create if statements for what was chosen ex:
-        //if they said yes to everything create if statement to create a password with every character
-        //if they said yes to uppercase, numbers, lowercase but no to specail characters then create a password with those 3 and so forth using if true statements
+//if they said yes to everything create if statement to create a password with every character
+//if they said yes to uppercase, numbers, lowercase but no to specail characters then create a password with those 3 and so forth using if true statements
 
 
 function generatePassword() {
 
-//Prompt asking how long user wants their password to be//
-const passwordLength = prompt("Pick bewteen numbers 8-128 to determine password lenght");
-//If number is not between 8-128, alert user to put a correct number.
-if (passwordLength<8 && passwordLength>128) {
-    alert("Password must be between 8-128 letters long.")
+    //Prompt asking how long user wants their password to be//
+    const passwordLength = prompt("Pick bewteen numbers 8-128 to determine password lenght");
     //if there is no number entered
-} else if (!passwordLength) {
-    alert("Must enter a number between 8-128 for password lenght")
-}
-//if the number is greater than/equal to 8 and less than/equal to 128
-else if ([passwordLength>==8 && passwordLength<==128]) {
-return;
-}
+    if (!passwordLength) {
+        alert("Must enter a number between 8-128 for password lenght");
+        //If number is not between 8-128, alert user to put a correct number.
+    } else if (passwordLength < 8 && passwordLength > 128) {
+        alert("Password must be between 8-128 letters long.");
 
-//Ask user with a confirm if they want to include upper case letters in their password
+        //if the number is greater than/equal to 8 and less than/equal to 128
+    } else if (passwordLength >= 8 && passwordLength <= 128) {
+        return;
+    }
 
-const upperCase = confirm('Would you like to include Upper Case letters in your password?');
-//true is clicked ok, false=cancel
-if (upperCase ===true) {
 
-}
+    //Ask user with a confirm if they want to include upper case letters in their password
+    const upperCase = confirm('Would you like to include Upper Case letters in your password?');
+    //true is clicked ok, false=cancel
+    if (upperCase === true) {
 
-const lowerCase = confirm('Would you like to include lower case numbers in your password?');
-const userspecialCharacters = confirm('Would you like to include special characters in your password?');
-const numbers =confirm('Would you like to include numbers in your password?');
+    }
+
+    const lowerCase = confirm('Would you like to include lower case numbers in your password?');
+    const userspecialCharacters = confirm('Would you like to include special characters in your password?');
+    const numbers = confirm('Would you like to include numbers in your password?');
 
 
 }
@@ -91,5 +91,5 @@ const numbers =confirm('Would you like to include numbers in your password?');
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
