178db09db1a93cf42e0dfbd56528e15a881867bd
Loryn <lorynhaykus@yahoo.com>
2021-05-06 15:41:30 -0600

    fixing mistakes

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ var generateBtn = document.querySelector("#generate");
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "?","/","|","{","}","[","]","'",".",",",";",":","~","`"];
-const password ="";
-const total =[];
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "?", "/", "|", "{", "}", "[", "]", "'", ".", ",", ";", ":", "~", "`"];
+const password = "";
+const total = [];
+
 
 
 function generatePassword() {
@@ -22,82 +23,89 @@ function generatePassword() {
     } else if (passwordLength < 8 && passwordLength > 128) {
         alert("Password must be between 8-128 letters long.");
     }
-    //if the number is greater than/equal to 8 and less than/equal to 128,
-    //ask the user to confirm other characters they want in their password
-    //} else if (passwordLength >= 8 && passwordLength <= 128) {
 
-    //}
 
     //ask user if they want to have lower case letters in password
     const lowerCase = confirm('Would you like to include Lower Case letters in your password?');
-  
+
     //ask user to confirm if they want special characters in password 
     const specialCharacters = confirm('Would you like to include Special Characters in your password?');
-  
+
     //ask user to confirm if they want numbers in their password
     const numbers = confirm('Would you like to include Numbers in your password?');
-  
+
     //ask users to confirm if they want uppercase letters in password
     const upperCase = confirm('Would you like to include Upper Case letters in your password?');
 }
+
 //if statement for if no characters were picked, alert user to pick characters
 if (!lowerCase && !specialCharacters && !numbers && !upperCase) {
-    alert("Password needs at least one lower caser, uppercase, special character and/or number")
-} 
-//if statement for if lowerCase letters were picked, then pick a random number and put it in the total array
- if (lowerCase===true) { 
-    password = password + Math.floor(Math.random()*lowerCase.length);
-    total= total.concat(lowerCase);
+    alert("Password needs at least one lower caser, uppercase, special character and/or number");
 
-} 
-//if statement for if numbers were picked, then pick a random number and add it to the total array
-if (numbers===true) {
-    password= password + Math.florr(Math.random()*numbers.length);
-    total=total.concat(numbers);
 }
+//if statement for if lowerCase letters were picked, then pick a random number and put it in the total array
+if (lowerCase === true) {
+    const characters = Math.floor(Math.random()*lowerCase.length);
+    password = password + lowerCase[characters];
+    total = total.concat(lowerCase);
+
+}
+
+//if statement for if numbers were picked, then pick a random number and add it to the total array
+if (numbers === true) {
+    const characters = Math.floor(Math.random()*numbers.length);
+    password = password + numbers[characters];
+    total = total.concat(numbers);
+}
+
 //if statement for if uppercase letters were picked, pick a random letter and add it to the total array
- if (upperCase===true) {
-     password = password + Math.floor(Math.random()*upperCase.length);
-     total=total.concat(upperCase);
- }
- //if statement for if special characters were picked, pick a randmo character and add it to the array
- if (specialCharacters===true) {
-     password = password + Math.floor(Math.random()*specialCharacters.length);
-     total=total.concat(specialCharacters);
- }
- //all characters from above combined and a for loop to randomly choose out of the total array
+if (upperCase === true) {
+    const characters = Math.floor(Math.random()*upperCase.length);
+    password = password + upperCase[characters];
+    total = total.concat(upperCase);
+}
 
- for (let i = 0; i < total.length; i++) {
-     password= password + Math.floor(Math.random()*total.length);  
- }
- console.log(password);
+//if statement for if special characters were picked, pick a randmo character and add it to the array
+if (specialCharacters === true) {
+    const characters = Math.floor(Math.random()*specialCharacters.lenght);
+    password = password + specialCharacters[characters];
+    total = total.concat(specialCharacters);
+}
+
+//all characters from above combined and a for loop to randomly choose out of the total array
+for (let i = 0; i < total.length; i++) {
+    const character = password + Math.floor(Math.random() * [total.length]);
+    password = total[(character)];
+console.log(password);
+}
 
 
+//var characters = Math.floor(Math.random() * lowerCase.length);
+//password = password + lowerCase[characters];
 
 
+// if (lowerCase===true){
+// password = password + Math.floor(Math.random()*lowerCase.length);
+//  total= total.concat(lowerCase);
+/// password = password + ////get random element from the lowerCase array
+/// total = total.concat(lowerCase)
 
-   // if (lowerCase===true){
-       // password = password + Math.floor(Math.random()*lowerCase.length);
-      //  total= total.concat(lowerCase);
-        /// password = password + ////get random element from the lowerCase array
-        /// total = total.concat(lowerCase)
-     
-   //  }
-    // if (...){
-        /// password = password + ////get random element from the ... array
-        /// total = total.concat(....)
-     
-  //   }
-    // if (...){
-        /// password = password + ////get random element from the ... array
-        /// total = total.concat(...)
-     
-   //  }
-   //  if (...){
-        /// password = password + ////get random element from the ... array
-        /// total = total.concat(...)
-     
-     //}
+//  }
+// if (...){
+/// password = password + ////get random element from the ... array
+/// total = total.concat(....)
+
+//   }
+// if (...){
+/// password = password + ////get random element from the ... array
+/// total = total.concat(...)
+
+//  }
+//  if (...){
+/// password = password + ////get random element from the ... array
+/// total = total.concat(...)
+
+//}
 
 
 
@@ -107,11 +115,11 @@ if (numbers===true) {
 //console.log(password)
 
 //for (let i = 0; i < lowerCase.length; i++) {
-  //  const characters = Math.floor(Math.random()* lowerCase.length);
-  //  password = password + lowerCase[characters];
+//  const characters = Math.floor(Math.random()* lowerCase.length);
+//  password = password + lowerCase[characters];
 
-  //  const element = arr];
-    
+//  const element = arr];
+
 //}
 //var characters = Math.floor(Math.random() * lowerCase.length);
 //password = password + lowerCase[characters];
@@ -123,7 +131,7 @@ if (numbers===true) {
 
 
 
- generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
 
 
@@ -139,9 +147,9 @@ function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-  
-  }
-  
+
+}
+
 
 
 
